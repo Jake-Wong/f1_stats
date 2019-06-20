fe9ea0a42cf00e509c85fa26b66b9efcd7051c90
Jake Wong <jakewongy@gmail.com>
2019-06-20 17:27:51 +0100

    Can now calculate the difference between qualifying laptimes

--- driver_data.py
+++ driver_data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 # Returns a dictionary containing the driver's full names in a given season
 def get_names(year):
@@ -14,3 +15,28 @@ def get_names(year):
         driver_names[driver["driverId"]] = driver["givenName"] + " " + driver["familyName"]
 
     return driver_names
+
+# Takes in a driver's data and returns their full name
+def format_name(driver_data):
+    return driver_data["givenName"] + " " + driver_data["familyName"]
+
+# Parses a lap time string into a time object
+def parse_laptime(laptime):
+    return datetime.datetime.strptime(laptime, "%M:%S.%f").time()
+
+# Formats a time object into a string with the suitable laptime format
+def format_time(time):
+    return ("%d:%d.%d" % (time.minute, time.second, time.microsecond))[:-3]
+
+# Finds the time difference between two given laptimes, returns a string
+def find_diff(laptime1, laptime2):
+    t2 = datetime.timedelta(minutes = laptime2.minute, seconds = laptime2.second, microseconds = laptime2.microsecond)
+    t1 = datetime.timedelta(minutes = laptime1.minute, seconds = laptime1.second, microseconds = laptime1.microsecond)
+    diff = t1 - t2
+
+    # Formats the string
+    if diff.total_seconds() >= 0:
+        delta = "+%.3f" % (diff.total_seconds())
+    else:
+        delta = "%.3f" % (diff.total_seconds())
+    return delta
--- qualy_results.py
+++ qualy_results.py
@@ -37,3 +37,35 @@ def cmp_qualy_results(driver1, driver2, season):
 
     driver_names = driver_data.get_names(season)
     print(driver_names[driver1] + " " + str(driver1_score) + "-" + str(driver2_score) + " " + driver_names[driver2])
+
+# Compares fastest qualifying times between drivers
+def cmp_fastest_lap(driver1, driver2, season, round):
+
+    # Stores the results of a qualifying session
+    r = requests.get("https://ergast.com/api/f1/" + str(season) + "/"+ str(round) + "/qualifying.json")
+    quali_results = json.loads(r.text)["MRData"]["RaceTable"]["Races"][0]["QualifyingResults"]
+
+    # Gets the driver's best times
+    for driver in quali_results:
+        if driver["Driver"]["driverId"] == driver1:
+            driver1_name = driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]
+            # Finds the fastest lap for driver1
+            if "Q3" in driver:
+                driver1_time = driver_data.parse_laptime(driver["Q3"])
+            elif "Q2" in driver:
+                driver1_time = driver_data.parse_laptime(driver["Q2"])
+            else:
+                driver1_time = driver_data.parse_laptime(driver["Q1"])
+        elif driver["Driver"]["driverId"] == driver2:
+            driver2_name = driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]
+            # Finds the fastest lap for driver2
+            if "Q3" in driver:
+                driver2_time = driver_data.parse_laptime(driver["Q3"])
+            elif "Q2" in driver:
+                driver2_time = driver_data.parse_laptime(driver["Q2"])
+            else:
+                driver2_time = driver_data.parse_laptime(driver["Q1"])
+
+    # Finds the time difference between driver 1's best time and driver 2's best time
+    lap_diff = driver_data.find_diff(driver1_time, driver2_time)
+    print(driver1_name + " " + driver_data.format_time(driver1_time) + "\n" + lap_diff + "\n" + driver2_name + " " + driver_data.format_time(driver2_time))
