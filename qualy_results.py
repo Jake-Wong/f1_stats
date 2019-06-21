84023e992bc8d20ab8e2d25c1165e96c0a4401e3
Jake Wong <jakewongy@gmail.com>
2019-06-21 12:26:18 +0100

    Improved formatting of qualifying lap comparison

--- qualy_results.py
+++ qualy_results.py
@@ -49,6 +49,7 @@ def cmp_fastest_lap(driver1, driver2, season, round):
     for driver in quali_results:
         if driver["Driver"]["driverId"] == driver1:
             driver1_name = driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]
+            driver1_pos = "P" + driver["position"]
             # Finds the fastest lap for driver1
             if "Q3" in driver:
                 driver1_time = driver_data.parse_laptime(driver["Q3"])
@@ -58,6 +59,7 @@ def cmp_fastest_lap(driver1, driver2, season, round):
                 driver1_time = driver_data.parse_laptime(driver["Q1"])
         elif driver["Driver"]["driverId"] == driver2:
             driver2_name = driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]
+            driver2_pos = "P" + driver["position"]
             # Finds the fastest lap for driver2
             if "Q3" in driver:
                 driver2_time = driver_data.parse_laptime(driver["Q3"])
@@ -68,4 +70,4 @@ def cmp_fastest_lap(driver1, driver2, season, round):
 
     # Finds the time difference between driver 1's best time and driver 2's best time
     lap_diff = driver_data.find_diff(driver1_time, driver2_time)
-    print(driver1_name + " " + driver_data.format_time(driver1_time) + "\n" + lap_diff + "\n" + driver2_name + " " + driver_data.format_time(driver2_time))
+    print(driver1_pos + " " + driver1_name + " " + driver_data.format_time(driver1_time) + "\n" + lap_diff + "\n" + driver2_pos + " " + driver2_name + " " + driver_data.format_time(driver2_time))
